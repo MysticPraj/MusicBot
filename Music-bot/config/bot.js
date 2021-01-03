23f5e01efe18b8203170172f8f983430c41f9998
Prajwal Gangawane <46399757+MysticPraj@users.noreply.github.com>
2021-01-04 00:05:50 +0530

    Add files via upload

---
+++ Music-bot/config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'Nzk1MjQ4Mjc3MDkxOTA5NjMy.X_GmrA.CyiB4G6RdmsaIFs8dLeZl8IOjCQ',
+        prefix: '$',
+        activity: 'ACTIVITY',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
